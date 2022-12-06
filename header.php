388558703148b6f712985c04e991e2e17bb0a137
ngocnguyenthe <92983323+ngocnguyenthe@users.noreply.github.com>
2022-12-07 00:27:54 +0700

    home

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="header">
+        <div class="col-third">    
+            <a href="">
+            <img src="https://www.coolmate.me/images/logo-coolmate-white.svg" width="95px" height="76px" alt="Logo_coolmate">
+            </a></div>
+        <div class="col-third">
+        <a href="./product/products.php"> Sản phẩm</a>
+        <a href="" class="">Về Coolmate</a>
+        <a href="" class="">Chọn Size</a>
+        </div>
+        <div class="pdr col-third">
+        <a class="" href="user/login.php"> <i class="ti-user"></i> Đăng nhập</a>
+        <a href="user/product_in_cart.php"> <i class="ti-shopping-cart"></i> Giỏ hàng</a>
+        </div>
+</div>
