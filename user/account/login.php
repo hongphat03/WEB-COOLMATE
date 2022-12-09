58d277faaa22d850165a0213c392687cc2eba87f
Hong Phat <nguyenhongphat22911@gmail.com>
2022-12-09 23:12:09 +0700

    ad5

--- user/account/login.php
+++ user/account/login.php
@@ -38,23 +38,25 @@
     <title>phan2_bai3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="../css/stylelogin.css">
 </head>
 <body>
-<div class="container p-5 my-5 border">
-   
-    <form method="post">       
-        <!-- email -->
-        <label for="email" class="form-text"><b>Email</b> </label>    <br>
-        <input type="email" name="email" class="form-control">    <br>
-    
-        <!-- password -->
-        <label for="password" class="form-text"><b>Password</b></label> <br>
-        <input type="password" name="password" class="form-control"> <br>
-    
-        <!-- submit -->
-        <button class="btn btn-success">Login</button>
-        <a href="register.php" class="btn btn-success">Register</a>
-    </form>
-</div>
+
+<div class="screenLogin">
+		<div class="centerScreen">
+			<div class="title">Đăng Nhập</div>
+			<form method="post">       
+				<!-- email -->
+				<input type="email" name="email" class="formControl" placeholder="Email/SĐT của bạn">    <br>
+			
+				<!-- password -->
+				<input type="password" name="password" class="formControl" placeholder="Mật khẩu"> <br>
+			
+				<!-- submit -->
+				<button class="btnlogin">Đăng nhập</button> <br>
+				<a href="./register.php" class="btnRegister">Đăng kí thành viên mới</a>
+			</form>
+		</div>
+	</div>
 </body>
 </html> 
--- user/account/register.php
+++ user/account/register.php
@@ -1,7 +1,7 @@
 
 <?php 
     session_start();
-    require_once('../database/dbhelper.php'); 
+    require_once('../../database/dbhelper.php'); 
     if(!empty($_POST)) {
         $name = $phone = $email = $password = $password2 = "";
         if(isset($_POST['name'])){
@@ -38,37 +38,27 @@
     <title>phan2_bai3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/stylelogin.css">
 </head>
 <body>
-<div class="container p-5 my-5 border">
-   
-    <form method="post">      
-        
-        <p style="font-size: 50px;" class="form-text text-center">REGISTER</p>
-        <!-- name  -->
-        <label for="firstName" class="form-text"><b>Ten Cua Ban</b></label> <br>
-        <input type="text" name="name" class="form-control"> <br>
-
-        <label for="secondName" class="form-text"><b>So Dien Thoai</b></label> <br>
-        <input type="text" name="phone" class="form-control"> <br>
-
-        <!-- email -->
-        <label for="email" class="form-text"><b>Email</b> </label>    <br>
-        <input type="text" name="email" class="form-control">    <br>
-        <!-- address -->
-        <label for="address" class="form-text"><b>Địa chỉ</b> </label>    <br>
-        <input type="text" name="address" class="form-control">    <br>
-        <!-- password -->
-        <label for="password" class="form-text"><b>Password</b></label> <br>
-        <input type="password" name="password" class="form-control"> <br>
-        <!-- nhap lai password  -->
-        <label for="password" class="form-text"><b>Password</b></label> <br>
-        <input type="password" name="password2" class="form-control"> <br>
-
-        <!-- submit -->
-        <button class="btn btn-success" >Dang ky</button>
-    </form>
-</div>
+    <div class="screenRegister">
+            <div class="centerScreen">
+                <div class="title">Đăng ký tài khoản</div>
+                <form method="post">       
+                    <!-- name  -->
+                    <input type="text" name="name" class="formControl" placeholder="Tên của bạn"> <br>
+                    <input type="text" name="phone" class="formControl" placeholder="SĐT của bạn"> <br>
+                    <!-- email -->
+                    <input type="text" name="email" class="formControl" placeholder="Email của bạn">    <br>
+                    <!-- password -->
+                    <input type="password" name="password" class="formControl" placeholder="Mật khẩu"> <br>
+                    <!-- nhap lai password  -->
+                    <input type="password" name="password2" class="formControl" placeholder="Nhập lại mật khẩu"> <br>
+                    <!-- submit -->
+                    <button class="btnlogin" >Đăng kí</button>
+                </form>
+            </div>
+        </div>
 </body>
 <script src="main.js"></script>
 </html> 
