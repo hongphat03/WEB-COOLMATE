d9ccaebdb6250d99fb582b559bbed2ab51e4582c
khanhminh51 <110477707+khanhminh51@users.noreply.github.com>
2022-12-14 21:09:59 +0700

    Update login.php

--- user/account/login.php
+++ user/account/login.php
@@ -37,7 +37,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/stylelogin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
- 
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="../../js/validation.js" type="text/javascript"></script>
+ 	<title>Document</title>
 
 </head>
 <body>
@@ -63,6 +65,5 @@
 		</div>
 	</div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../../js/validation.js" type="text/javascript"></script>
+
 </html> 
