9c326b01f5378f51e805e798d28d928ced2cf20c
khanhminh51 <110477707+khanhminh51@users.noreply.github.com>
2022-12-14 21:17:04 +0700

    Update main.php

--- view/main.php
+++ view/main.php
@@ -14,13 +14,13 @@
     </div>
     <div class="slideshow-container">
         <div class="mySlides fade">
-            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/D08354C9-55C1-4D2E-8075-F2C520F3A06E.png" width="100%">
+            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/D08354C9-55C1-4D2E-8075-F2C520F3A06E.png" width="100%" alt="">
         </div>
         <div class="mySlides fade">
-            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/Banner-hld-VOUCHER_74.jpg" width="100%">
+            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/Banner-hld-VOUCHER_74.jpg" width="100%" alt="">
         </div>
         <div class="mySlides fade">
-            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/marvelDesktop.jpg" style="width:100%">
+            <img src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/December2022/marvelDesktop.jpg" style="width:100%" alt="">
         </div>
     </div>
 <br>
@@ -39,7 +39,7 @@
     </div>
     <div class="khampha">
         <img class="nav_nen" width="100%" src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/November2022/mceclip2_68.png" alt="">
-        <img class="nav_chinh" width="1280px";width="680px"; src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/November2022/mceclip0_33.png" alt="">
+        <img class="nav_chinh" width="1280px" height="680px"  src="https://media.coolmate.me/cdn-cgi/image/width=1920,quality=90,format=auto/uploads/November2022/mceclip0_33.png" alt="">
 
     </div>
     <div class="nav_main container">
